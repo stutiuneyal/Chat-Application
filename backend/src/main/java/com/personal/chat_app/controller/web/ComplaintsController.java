8e791d97fb46f48e996b83e6fe009051af43297d
Stuti Uniyal <stutiuniyal27@gmail.com>
2025-11-03 02:07:02 +0530

    Stuti Homework

---
+++ backend/src/main/java/com/personal/chat_app/controller/web/ComplaintsController.java
@@ -0,0 +1,14 @@
+package com.personal.chat_app.controller.web;
+
+/*
+ * TODO:
+ * 1. A user should be able to raise complaints against other users in a room (default: Pending)
+ * 2. Admins should be able to view all complaints depending on the status for the room, 
+ * and the user should be able to view only their raised complaints and their statuses.
+ * 3. Admin can Approve/Reject the complaint.
+ *         - If approved the set of users against whim the complaint was raised should be removed from the room.
+ */
+
+public class ComplaintsController {
+
+}
